2bcf993ca00630e08432dc216953ace70e4f2575
Raymond Morgan <raymondmo310@gmail.com>
2017-01-20 20:05:23 +0000

    geo pattern with vert circles

--- Geometric Pattern/main.js
+++ Geometric Pattern/main.js
@@ -1,22 +1,25 @@
 var NUM_CIRCLES = 12
     var circleDiameter;
+    var circleRadius;
+
 
 function setup() {
     createCanvas(480,600);
     circleDiameter = width/NUM_CIRCLES;
+    circleRadius = circleDiameter/2;
 }
 
 function draw() {
    var y = 0;
-   while(y <= height) {
+   while (y <= height) {
        
 
      var x = 0;
      while (x <= width) {
-       ellipse(x, 0, circleDiameter, circleDiameter);
+       ellipse(x, y, circleDiameter, circleDiameter);
        x = x + circleDiameter;
      }
     
-     y = y + circleDiameter;
+     y = y + circleRadius;
     }
 }
