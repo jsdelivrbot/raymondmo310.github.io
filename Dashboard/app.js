de07aa8cf0740d08233c28a63f74d9d8943f10ad
Raymond Morgan <raymondmo310@gmail.com>
2017-03-20 18:56:07 +0000

    working final

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -12,14 +12,12 @@ function loadWeather() {
     var weather = $("#weather");
     var url = "https://api.forecast.io/forecast/";
     var apiKey = "85a83c26a1dd4afa7644b05d99d91d15"
-}
 
 function success(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     
-    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
-function(data) {
+    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
     weather.text("Based on your current location, it is " + data.currently.temperature + "° F right now");
 
     });
@@ -28,24 +26,27 @@ function(data) {
 function error() {
     alert("Unable to retrieve your location for weather");
 }
-navigator.geolocation.getCurrentPosition(success, error);
+    
+    navigator.geolocation.getCurrentPosition(success, error);
 
-weather.text("fetching weather...");
+    weather.text("fetching weather...");
+}
+
 
 function loadNews() {
-    var news = $("#news");
-    var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
-    var apiKey = "367b5efb65384d97b42dbc82ef5d0005";
-    
-    $.getJSON(url + apiKey, function(date) {
-        var titles = data.articles.map(function(articles) {
-            return "<a href='" + articles.url + "'>" + articles.title + "</a>";
-        });
-        
-        news.html(titles.join("<br><br>"));
+  var news = $("#news");
+  var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
+  var apiKey = "367b5efb65384d97b42dbc82ef5d0005";
+  
+   $.getJSON(url + apiKey, function(data) {
+    var titles = data.articles.map(function(articles) {
+      return "<a href='" + articles.url + "'>" + articles.title + "</a>";
     });
-    
-    news.text("fetching news...")
+
+    news.html(titles.join("<br><br>"));
+  });
+
+  news.text("fetching news...");
 }
 
 loadDate();
